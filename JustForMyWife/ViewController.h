103f7ba0a312addf488e724d48efe4d0c0421221
Genglei <1247759034@qq.com>
2016-03-09 10:39:16 +0800

    Initial Commit

---
+++ JustForMyWife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JustForMyWife
+//
+//  Created by geng lei on 16/3/9.
+//  Copyright © 2016年 com.fengche.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JustForMyWife/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JustForMyWife
+//
+//  Created by geng lei on 16/3/9.
+//  Copyright © 2016年 com.fengche.cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
